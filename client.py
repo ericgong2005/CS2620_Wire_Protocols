8b2faff21b6191dae3c3b78476a235e950c37fae
Charlie Chen <charliechen@dhcp-10-250-187-147.harvard.edu>
2025-02-07 13:26:07 -0500

    test code from lecture

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 54400
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        message = input("Enter a message to send to the server: ")
+        if message == "exit":
+            break
+        message = message.encode("utf-8")
+        s.sendall(message)
+        data = s.recv(1024)
+        data = data.decode("utf-8")
+        print(f"Received: {data}")
